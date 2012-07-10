f7afea948afa9fb2c8c8e11bdf0f7c0227be3299
Min <minjhe@gmail.com>
2012-07-10 14:05:27 +0800

    update rss-reader

--- rss-reader/header.php
+++ rss-reader/header.php
@@ -7,7 +7,7 @@ $img_base_url = "https://github.com/JeffreyWay/Tuts----jQuery-Mobile-RSS-Reader/
 <head>
   <title><?php echo isset($siteName) ? ucWords($siteName) : 'Tuts+'; ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="apple-touch-icon" href="<?php print $img_base_url;?>/tutsTouchIcon.png" />
+  <link rel="apple-touch-icon" href="<?php print $img_base_url;?>/tutsTouchIcon.jpg" />
   <link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.css" />
   <link rel="stylesheet" href="custom.css" />
   <script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
--- rss-reader/index.php
+++ rss-reader/index.php
@@ -10,44 +10,34 @@
 <div data-role="content">
 	<ul data-role="listview" data-theme="c" data-dividertheme="d" data-counttheme="e">
   	<li>
-    	<img src="<?php print $img_base_url; ?>/ntLogo.jpg" alt="Nettuts" class="ui-li-icon" />
-    	<a href="site.php?siteName=nettuts"> Nettuts+ </a>
+      <a href="site.php?siteName=nettuts"><img src="<?php print $img_base_url; ?>/ntLogo.jpg" alt="Nettuts" class="ui-li-icon"> Nettuts+ </a>
   	</li>
   	<li>
-    	<img src="<?php print $img_base_url; ?>/psdLogo.jpg" alt="Psdtuts" class="ui-li-icon" />
-    	<a href="site.php?siteName=psdtuts"> Psdtuts+ </a>
+    	<a href="site.php?siteName=psdtuts"><img src="<?php print $img_base_url; ?>/psdLogo.jpg" alt="Psdtuts" class="ui-li-icon"> Psdtuts+ </a>
   	</li>
   	<li>
-    	<img src="<?php print $img_base_url; ?>/vectorLogo.jpg" alt="Vectortuts+" class="ui-li-icon" />
-    	<a href="site.php?siteName=vectortuts"> Vectortuts+ </a>
+      <a href="site.php?siteName=vectortuts"><img src="<?php print $img_base_url; ?>/vectorLogo.jpg" alt="Vectortuts" class="ui-li-icon"> Vectortuts+ </a>
   	</li>
   	<li>
-    	<img src="<?php print $img_base_url; ?>/mobileLogo.png" alt="Mobiletuts+" class="ui-li-icon" />
-    	<a href="site.php?siteName=mobiletuts"> Mobiletuts+ </a>
+      <a href="site.php?siteName=mobiletuts"><img src="<?php print $img_base_url; ?>/mobileLogo.png" alt="Mobiletuts" class="ui-li-icon"> Mobiletuts+ </a>
   	</li>
   	<li>
-    	<img src="<?php print $img_base_url; ?>/aeLogo.jpg" alt="Aetuts+" class="ui-li-icon" />
-    	<a href="site.php?siteName=aetuts"> Aetuts+ </a>
+      <a href="site.php?siteName=aetuts"><img src="<?php print $img_base_url; ?>/aeLogo.jpg" alt="Aetuts" class="ui-li-icon"> Aetuts+ </a>
   	</li>
   	<li>
-    	<img src="<?php print $img_base_url; ?>/photoLogo.jpg" alt="Phototuts+" class="ui-li-icon" />
-    	<a href="site.php?siteName=phototuts"> Phototuts+ </a>
+      <a href="site.php?siteName=phototuts"><img src="<?php print $img_base_url; ?>/photoLogo.jpg" alt="Phototuts" class="ui-li-icon"> Phototuts+ </a>
   	</li>
   	<li>
-    	<img src="<?php print $img_base_url; ?>/cgLogo.jpg" alt="Cgtuts+" class="ui-li-icon" />
-    	<a href="site.php?siteName=cgtuts"> Cgtuts+ </a>
+      <a href="site.php?siteName=cgtuts"><img src="<?php print $img_base_url; ?>/cgLogo.jpg" alt="Cgtuts" class="ui-li-icon"> Cgtuts+ </a>
   	</li>
   	<li>
-    	<img src="<?php print $img_base_url; ?>/audioLogo.jpg" alt="Audiotuts+" class="ui-li-icon" />
-    	<a href="site.php?siteName=audiotuts"> Audiotuts+ </a>
+      <a href="site.php?siteName=audiotuts"><img src="<?php print $img_base_url; ?>/audioLogo.jpg" alt="Audiotuts" class="ui-li-icon"> Audiotuts+ </a>
   	</li>
   	<li>
-    	<img src="<?php print $img_base_url; ?>/activeLogo.jpg" alt="Activetuts+" class="ui-li-icon" />
-    	<a href="site.php?siteName=flashtuts"> Activetuts+ </a>
+      <a href="site.php?siteName=flashtuts"><img src="<?php print $img_base_url; ?>/activeLogo.jpg" alt="Activetuts" class="ui-li-icon"> Activetuts+ </a>
   	</li>
   	<li>
-    	<img src="<?php print $img_base_url; ?>/wdLogo.jpg" alt="Webdesigntuts+" class="ui-li-icon" />
-    	<a href="site.php?siteName=webdesigntutsplus"> Webdesigntuts+ </a>
+      <a href="site.php?siteName=webdesigntutsplus"><img src="<?php print $img_base_url; ?>/wdLogo.jpg" alt="Webdesigntuts" class="ui-li-icon"> Webdesigntuts+ </a>
   	</li>
 	</ul>
 </div>
